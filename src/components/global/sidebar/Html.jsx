c8ffb6b172c13cd5a46335c414f4b2f77db6cc07
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-03-21 16:55:33 +0530

    sidebar changes

--- src/components/global/sidebar/Html.jsx
+++ src/components/global/sidebar/Html.jsx
@@ -19,9 +19,9 @@ const Html = ({ ListItemLink, tabclass, urlAllow, route, isOpen }) => {
   const location = useLocation();
 
 
-  const menus=[
+  const menus = [
     {
-      name:'Main Menu',
+      name: 'Main Menu',
     },
     // {
     //   name:'Dashboard',
@@ -30,118 +30,118 @@ const Html = ({ ListItemLink, tabclass, urlAllow, route, isOpen }) => {
     //   key:'dashboard',
     // },
     {
-      name:'User Management',
-      icon:<FiUsers className="text-[#5577FF] shrink-0 text-lg" />,
-      key:'roles,users',
-      tab:'user',
-      menu:[
+      name: 'User Management',
+      icon: <FiUsers className="text-[#5577FF] shrink-0 text-lg" />,
+      key: 'roles,users',
+      tab: 'user',
+      menu: [
         {
-          name:'Roles',
-          url:'/roles',
-          key:'roles'
+          name: 'Roles',
+          url: '/roles',
+          key: 'roles'
         },
         {
-          name:'Users',
-          url:'/users',
-          key:'users'
+          name: 'Users',
+          url: '/users',
+          key: 'users'
         },
       ]
     },
     {
-      name:'Catalogue',
+      name: 'Catalogue',
       icon: <GrCatalogOption className="text-[#fd71af] shrink-0 text-lg" />,
-      key:'categories,types',
-      tab:'catalogue',
-      menu:[
+      key: 'categories,types',
+      tab: 'catalogue',
+      menu: [
         {
-          name:'Types',
-          url:'/types',
-          key:'types'
+          name: 'Types',
+          url: '/types',
+          key: 'types'
         },
         {
-          name:'Categories',
-          url:'/categories',
-          key:'categories'
+          name: 'Categories',
+          url: '/categories',
+          key: 'categories'
         },
       ]
     },
     {
-      name:'Subscription Plan',
+      name: 'Subscription Plan',
       icon: <i className="material-icons text-[#ffc800] shrink-0 text-lg" >subscriptions</i>,
-      key:'features,plans',
-      tab:'plan',
-      menu:[
+      key: 'features,plans',
+      tab: 'plan',
+      menu: [
         {
-          name:'Features',
-          url:'/features',
-          key:'features'
+          name: 'Features',
+          url: '/features',
+          key: 'features'
         },
         {
-          name:'Plans',
-          url:'/plans',
-          key:'plans'
-        },
-      ]
-    },
-    {
-      name:'Currency',
-      icon:<i className="material-icons text-[#00b884] shrink-0 text-lg" title="">currency_exchange</i>,
-      url:'/currency',
-      key:'currency',
-    },
-    {
-      name:'Customers',
-      icon:<RiPresentationLine className="text-[#7b68ee] shrink-0 text-lg" />,
-      key:'customer,skill-roles',
-      tab:'customer',
-      menu:[
-        {
-          name:'Customer Roles',
-          url:'/skill-roles',
-          key:'skillRoles'
-        },
-        {
-          name:'Customers',
-          url:'/customer',
-          key:'customer'
+          name: 'Plans',
+          url: '/plans',
+          key: 'plans'
         },
       ]
     },
     {
-      name:'Participant Type',
-      icon:<RiHome6Line className="text-[#00b884] shrink-0 text-lg" />,
-      url:'/participant-type',
-      key:'participant-type',
+      name: 'Currency',
+      icon: <i className="material-icons text-[#00b884] shrink-0 text-lg" title="">currency_exchange</i>,
+      url: '/currency',
+      key: 'currency',
     },
     {
-      name:'Skills',
-      icon:<RiHome6Line className="text-[#00b884] shrink-0 text-lg" />,
-      key:'skills',
-      url:'/skills',
+      name: 'Customers',
+      icon: <RiPresentationLine className="text-[#7b68ee] shrink-0 text-lg" />,
+      key: 'customer,skill-roles',
+      tab: 'customer',
+      menu: [
+        {
+          name: 'Customer Roles',
+          url: '/skill-roles',
+          key: 'skillRoles'
+        },
+        {
+          name: 'Customers',
+          url: '/customer',
+          key: 'customer'
+        },
+      ]
     },
     {
-      name:'Group',
-      icon:<span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">groups</span>,
-      url:'/group',
-      key:'group',
+      name: 'Participant Type',
+      icon: <RiHome6Line className="text-[#00b884] shrink-0 text-lg" />,
+      url: '/participant-type',
+      key: 'participant-type',
     },
     {
-      name:'Certification',
-      icon:<span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">workspace_premium</span>,
-      url:'/certification',
-      key:'certification',
+      name: 'Skills',
+      icon: <RiHome6Line className="text-[#00b884] shrink-0 text-lg" />,
+      key: 'skills',
+      url: '/skills',
     },
     {
-      name:'Content Management',
-      icon:<span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">content_paste</span>,
-      url:'/content',
-      key:'content',
+      name: 'Group',
+      icon: <span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">groups</span>,
+      url: '/group',
+      key: 'group',
     },
     {
-      name:'Events',
-      icon:<span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">workspace_premium</span>,
-      url:'/event',
-      key:'event',
+      name: 'Certification',
+      icon: <span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">workspace_premium</span>,
+      url: '/certification',
+      key: 'certification',
+    },
+    {
+      name: 'Content Management',
+      icon: <span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">content_paste</span>,
+      url: '/content',
+      key: 'content',
+    },
+    {
+      name: 'Events',
+      icon: <span class="material-symbols-outlined text-[#ffc800] shrink-0 text-lg">workspace_premium</span>,
+      url: '/event',
+      key: 'event',
     },
   ]
 
@@ -151,86 +151,86 @@ const Html = ({ ListItemLink, tabclass, urlAllow, route, isOpen }) => {
       <div className={`px-[8px] ${isOpen && styles.sm_sidebar}`} component="siderbar">
 
         <ul className="space-y-2 px-2" >
-          {menus.map(itm=>{
+          {menus.map(itm => {
             return <>
-            {itm.icon?<>
-              <li>
-            {itm.menu?<>
-              <Disclosure as="div" defaultOpen={tabclass(itm.tab)}>
-              {({ open }) => (
-                <>
-                  <Tooltip placement="right" title={itm.name}>
-                    <Disclosure.Button className="w-full p-2.5 rounded-md flex items-center justify-between text-[#4A545E]  hover:!text-[#5577FF] gap-[12px] hover:bg-[#5577FF]/10 transition-all duration-300">
+              {itm.icon ? <>
+                <li>
+                  {itm.menu ? <>
+                    <Disclosure as="div" defaultOpen={tabclass(itm.tab)}>
+                      {({ open }) => (
+                        <>
+                          <Tooltip placement="right" title={itm.name}>
+                            <Disclosure.Button className="w-full p-2.5 rounded-md flex items-center justify-between text-[#4A545E]  hover:!text-[#5577FF] gap-[12px] hover:bg-[#5577FF]/10 transition-all duration-300">
 
-                      <span className="text-sm font-normal text-inherit flex items-center gap-[12px] crm">
-                      {itm.icon}
-                        <span className=" text-inherit leading-none sidebar_text"> {itm.name}</span>
+                              <span className="text-sm font-normal text-inherit flex items-center gap-[12px] crm">
+                                {itm.icon}
+                                <span className=" text-inherit leading-none sidebar_text"> {itm.name}</span>
 
-                      </span>
+                              </span>
 
-                      <TiArrowSortedDown
-                        className={`${open ? "" : "-rotate-90 transform"
-                          } h-4 w-4 transition-all duration-500  text-[#7E8B99]`}
-                      />
-                    </Disclosure.Button>
-                  </Tooltip>
-                  <Transition
-                    enter="transition duration-300 ease-in-out"
-                    enterFrom="transform scale-95 opacity-0"
-                    enterTo="transform scale-300 opacity-300"
-                    leave="transition duration-300 ease-in-out"
-                    leaveFrom="transform scale-300 opacity-300"
-                    leaveTo="transform scale-95 opacity-0">
-                    <Disclosure.Panel className="pl-[30px] mt-[4px] ">
-                      <ul className="space-y-2">
-                        {itm.menu.map(sitm=>{
-                          return <>
-                           {urlAllow(sitm.key) ? <li> <NavLink className={(isActive) =>
-                          "p-2.5 rounded-md block text-sm font-normal text-[#4A545E] cursor-pointer hover:!text-[#5577FF] hover:bg-[#5577FF]/10 !no-underline transition-all " +
-                          (location?.pathname == sitm.url &&
-                            " !text-[#5577FF] !bg-[#5577FF]/10 !font-medium")
-                        } to={sitm.url}>
+                              <TiArrowSortedDown
+                                className={`${open ? "" : "-rotate-90 transform"
+                                  } h-4 w-4 transition-all duration-500  text-[#7E8B99]`}
+                              />
+                            </Disclosure.Button>
+                          </Tooltip>
+                          <Transition
+                            enter="transition duration-300 ease-in-out"
+                            enterFrom="transform scale-95 opacity-0"
+                            enterTo="transform scale-300 opacity-300"
+                            leave="transition duration-300 ease-in-out"
+                            leaveFrom="transform scale-300 opacity-300"
+                            leaveTo="transform scale-95 opacity-0">
+                            <Disclosure.Panel className="pl-[30px] mt-[4px] ">
+                              <ul className="space-y-2">
+                                {itm.menu.map(sitm => {
+                                  return <>
+                                    {urlAllow(sitm.key) ? <li> <NavLink className={(isActive) =>
+                                      "p-2.5 rounded-md block text-sm font-normal text-[#4A545E] cursor-pointer hover:!text-[#5577FF] hover:bg-[#5577FF]/10 !no-underline transition-all " +
+                                      (location?.pathname == sitm.url &&
+                                        " !text-[#5577FF] !bg-[#5577FF]/10 !font-medium")
+                                    } to={sitm.url}>
 
-                          <span className="text-inherit leading-none sidebar_text" title={sitm.name}> {sitm.name}</span>
-                        </NavLink></li> : null}
-                          </>
-                        })}
-                      </ul>
+                                      <span className="text-inherit leading-none sidebar_text" title={sitm.name}> {sitm.name}</span>
+                                    </NavLink></li> : null}
+                                  </>
+                                })}
+                              </ul>
 
 
-                    </Disclosure.Panel>
-                  </Transition>
-                </>
-              )}
-            </Disclosure>
-            </>:<>
-            {urlAllow(itm.key) ? <>
-              <Tooltip placement="right" title={itm.name}>
-                <NavLink
-                  to={itm.url}
-                  className={(isActive) =>
-                    "p-2.5 rounded-md flex items-center gap-[12px] text-sm font-normal text-[#4A545E] hover:!text-[#00b884] hover:bg-[#00b884]/10 !no-underline transition-all " +
-                    (isActive && " !text-[#00b884] !bg-[#EDECF9] !font-medium")
-                  }>
+                            </Disclosure.Panel>
+                          </Transition>
+                        </>
+                      )}
+                    </Disclosure>
+                  </> : <>
+                    {urlAllow(itm.key) ? <>
+                      <Tooltip placement="right" title={itm.name}>
+                        <NavLink
+                          to={itm.url}
+                          className={(isActive) =>
+                            "p-2.5 rounded-md flex items-center gap-[12px] text-sm font-normal text-[#4A545E] hover:!text-[#00b884] hover:bg-[#00b884]/10 !no-underline transition-all " +
+                            (location?.pathname == itm.url && " !text-[#00b884] !bg-[#EDECF9] !font-medium")
+                          }>
 
-                  {itm.icon}
-                  <span className="text-inherit leading-none sidebar_text">{itm.name}</span>
+                          {itm.icon}
+                          <span className="text-inherit leading-none sidebar_text">{itm.name}</span>
 
-                </NavLink>
-              </Tooltip>
-            </> : <></>}
-            </>}
-          </li>
-            </>:<>
-            <li>
-            <h6
-            className={`${isOpen ? "py-[12px] text-center" : "p-[12px]"
-              } text-xs font-medium text-[#7E8B99] mt-[12px]`}>
-            <span className=" sidebar_text"> {itm.name} </span>
-          </h6>
-            </li>
-            </>}
-            
+                        </NavLink>
+                      </Tooltip>
+                    </> : <></>}
+                  </>}
+                </li>
+              </> : <>
+                <li>
+                  <h6
+                    className={`${isOpen ? "py-[12px] text-center" : "p-[12px]"
+                      } text-xs font-medium text-[#7E8B99] mt-[12px]`}>
+                    <span className=" sidebar_text"> {itm.name} </span>
+                  </h6>
+                </li>
+              </>}
+
             </>
           })}
 
