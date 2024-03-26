8668c9b3ea3662584999940eb71c1e686745ae00
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-03-26 16:05:47 +0530

    design

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -89,7 +89,7 @@ const Login = () => {
         if (res.data.two_factor_email_sent || step==1) {
           setStep(2)
           setRes(res.data)
-          setLogin(res.data)
+          // setLogin(res.data)
         } else {
           setLogin(resp)
         }
