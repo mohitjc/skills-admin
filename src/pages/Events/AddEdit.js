7daff4e6f9bfc42b8fa70510fca62127ef15cb7a
Frontend RVRenterCenter <frontend@rvrentercenter.com>
2024-04-18 10:35:11 +0500

    chnages for the dtata

--- src/pages/Events/AddEdit.js
+++ src/pages/Events/AddEdit.js
@@ -263,14 +263,14 @@ useEffect(() => {
                                 required
                             />
                         </div>
-<div className="col-md-6 mb-3">
-    <label>Group</label>
-    <select  value={form.groupId}  onChange={(e) => handleSelectValue(e)}>
-    <option value=""  disabled selected>select Group</option>
-        {data?.map((ele)=>{ return(<><option key={ele.id} value={ele.id}>{ele?.name}</option></>)})}
-        
-    </select>
-</div>
+                        <div className="col-md-6 mb-3">
+                            <label className="block">Group</label>
+                            <select  className="relative shadow-box bg-white focus:outline-none w-full rounded-lg h-10 flex items-center gap-2 overflow-hidden px-2" value={form.groupId}  onChange={(e) => handleSelectValue(e)}>
+                            <option value=""  disabled selected>select Group</option>
+                                {data?.map((ele)=>{ return(<><option key={ele.id} value={ele.id}>{ele?.name}</option></>)})}
+                                
+                            </select>
+                        </div>
 
                         <div className="col-span-full mb-3">
                             <FormControl
