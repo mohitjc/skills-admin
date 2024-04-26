3a78f31d9a4bd31960abf6d60a85c985f8c27458
JC Software Solution <engjcsoftwaresolution@gmail.com>
2024-04-26 19:05:10 +0530

    changes

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,21 +1,6 @@
 let host = document.location.host;
 let apiUrl ='https://skillprobackend.azurewebsites.net/'
-// let apiUrl ='http://192.168.1.52:7071/'
-// if(host.includes('localhost')){
-//   // apiUrl='http://portal.jcsoftwaresolution.in:6068/'
-//   apiUrl='http://192.168.1.52:7071/'
-// }
-
 const environment = {
-    // api: apiUrl,
-    // map_api_key:'AIzaSyCbRhC6h9Pp43-5t_Knyrd_ewAdLMIJtCg',
-    // planTypeId:'65ead4e65cfbfd7a03ed102f',
-    // userRoleId:'65eac23ceac028f4dbfb1fbc',
-    // customerRoleId:'66026ea2f105ac23ab31e48e',
-    // glRoleId:'65fc4a1e2e22cff912335e12',
-    // adminRoleId:'65eab1d84e01e43033dc2438',
-    // professionType:'65fa7ad79ef95c639effcf1c',
-    // productTypeId:'64a7d198fa039f179c0320ca'
     api: apiUrl,
     sasKey:'sp=racwdli&st=2024-04-25T09:28:38Z&se=2024-04-30T17:28:38Z&sv=2022-11-02&sr=c&sig=qfRZ%2B%2Bb7z9aBHmlcE2XM0XQDIKa2WLcieACpb%2B4S9PU%3D',
     sasurl:'https://skillprobackendstore.blob.core.windows.net',
