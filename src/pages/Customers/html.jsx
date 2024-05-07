d2f688936dcd19dd54af490e31e2190e5bccd194
Ritika <ritika@jcsoftwaresolution.com>
2024-05-07 18:59:33 +0530

    node 14 to node 20 version change

--- src/pages/Customers/html.jsx
+++ src/pages/Customers/html.jsx
@@ -13,7 +13,7 @@ import { BsEnvelope, BsTrash3 } from "react-icons/bs";
 import Table from '../../components/Table';
 import shared from './shared';
 import environment from '../../environment';
-import  Modal from "../../components/common/Modal";
+import Modal from "../../components/common/Modal";
 import SendEmail from './SendEmail';
 import ApiClient from '../../methods/api/apiClient';
 
@@ -42,28 +42,28 @@ const Html = ({
         })
     }
 
-    const [modal,setModel]=useState(false)
-    const [item,setItem]=useState()
+    const [modal, setModel] = useState(false)
+    const [item, setItem] = useState()
 
     const columns = [
         {
             key: 'fullName', name: 'Name', sort: true,
             render: (itm) => {
                 return <>
-                <div className='user_detail'>
-                                                    <img src={methodModel.userImg(itm.image)} className="user_imgs" />
-                                                    <div className='user_name'>
-                                                        <h4 className='user'>
-                                                            {itm.fullName}
-                                                        </h4>
-                                                        <p className='user_info'>
-                                                            {itm.email}
-                                                        </p>
-                                                        <p className='user_info'>
-                                                            {itm.mobileNo ? <>+{itm.mobileNo}</> : ''}
-                                                        </p>
-                                                    </div>
-                                                </div>
+                    <div className='user_detail'>
+                        <img src={methodModel.userImg(itm.image)} className="user_imgs" />
+                        <div className='user_name'>
+                            <h4 className='user'>
+                                {itm.fullName}
+                            </h4>
+                            <p className='user_info'>
+                                {itm.email}
+                            </p>
+                            <p className='user_info'>
+                                {itm.mobileNo ? <>+{itm.mobileNo}</> : ''}
+                            </p>
+                        </div>
+                    </div>
                 </>
             }
         },
@@ -84,7 +84,7 @@ const Html = ({
             }
         },
         {
-            key: 'createdAt', name: 'Created At',sort: true,
+            key: 'createdAt', name: 'Created At', sort: true,
             render: (row) => {
                 return <>
                     {datepipeModel.datetime(row?.createdAt)}
@@ -111,23 +111,23 @@ const Html = ({
             key: 'Approve/Decline', name: 'Approve/Decline',
             render: (row) => {
                 return <>
-                {row.customerRole==environment.glRoleId?<>
-                {row.verifiedGroupLeader?<>
-                    {row.verifiedGroupLeader=='approved'?<>
+                    {row.customerRole == environment.glRoleId ? <>
+                        {row.verifiedGroupLeader ? <>
+                            {row.verifiedGroupLeader == 'approved' ? <>
+                                <div className='text-green-600'>Approved</div>
+                            </> : <>
+                                <div className='text-red-600'>Declined</div>
+                            </>}
+                        </> : <>
+                            <div className='flex gap-2'>
+                                <button type="button" onClick={() => approveDecline(row, 'approved')} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs px-5 py-2 px-3 text-center">Approve</button>
+                                <button type="button" onClick={() => approveDecline(row, 'declined')} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xs py-2 px-3 text-center">Decline</button>
+                            </div>
+                        </>}
+
+                    </> : <>
                         <div className='text-green-600'>Approved</div>
-                    </>:<>
-                    <div className='text-red-600'>Declined</div>
                     </>}
-                </>:<>
-                <div className='flex gap-2'>
-                <button type="button" onClick={()=>approveDecline(row,'approved')} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-xs px-5 py-2 px-3 text-center">Approve</button>
-                <button type="button" onClick={()=>approveDecline(row,'declined')} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xs py-2 px-3 text-center">Decline</button>
-                </div>
-                </>}
-                    
-                </>:<>
-                <div className='text-green-600'>Approved</div>
-                </>}
                 </>
             }
         },
@@ -150,11 +150,11 @@ const Html = ({
             render: (itm) => {
                 return <>
                     <div className="flex items-center justify-start gap-1.5">
-                    <Tooltip placement="top" title="View">
-                                <a className="border cursor-pointer border-[#ff7641] hover:opacity-70 rounded-lg bg-[#ff764114] w-10 h-10 !text-primary flex items-center justify-center text-xl" onClick={e => view(itm.id)}>
+                        <Tooltip placement="top" title="View">
+                            <a className="border cursor-pointer border-[#ff7641] hover:opacity-70 rounded-lg bg-[#ff764114] w-10 h-10 !text-primary flex items-center justify-center text-xl" onClick={e => view(itm.id)}>
                                 <span class="material-symbols-outlined">visibility</span>
-                                </a>
-                            </Tooltip>
+                            </a>
+                        </Tooltip>
                         {isAllow(`edit${shared.check}`) ?
                             <Tooltip placement="top" title="Edit">
                                 <a className="border cursor-pointer border-[#ff7641] hover:opacity-70 rounded-lg bg-[#ff764114] w-10 h-10 !text-primary flex items-center justify-center text-xl" onClick={e => edit(itm.id)}>
@@ -175,7 +175,7 @@ const Html = ({
         },
     ]
 
-    const sendEmail=(itm)=>{
+    const sendEmail = (itm) => {
         // setItem(itm)
         // setModel(true)
         window.open(`mailto:${itm.email}`)
@@ -216,44 +216,44 @@ const Html = ({
 
     return (
         <>
-         <Layout>
-            <div className="flex justify-between items-center mb-6">
+            <Layout>
+                <div className="flex justify-between items-center mb-6">
 
 
-                <div>
-                    <h3 className="text-2xl font-semibold text-[#111827]">
-                    {shared.title}
-                    </h3>
-                    <p class="text-sm font-normal text-[#75757A]">Here you can see all about your {shared.title}</p>
-                </div>
-
-
-                <div className="flex">
-                    <div className="buttons_Section filterFlex">
-
-                        <button className="!px-2.5 text-[#3C3E49] text-sm font-normal py-2.5 flex items-center justify-center gap-2 bg-[#fff] rounded-lg shadow-btn hover:bg-[#F3F2F5] border border-[#D0D5DD] transition-all focus:ring-2 ring-[#F1F2F3] disabled:bg-[#F3F2F5] disabled:cursor-not-allowed mr-3" onClick={() => exportfun()}>
-                            <PiFileCsv className="text-typo text-xl" />  Export CSV
-                        </button>
-                        {isAllow(`add${shared.check}`) ? <>
-                            <Link className="bg-primary leading-10 mr-3 h-10 flex items-center shadow-btn px-6 hover:opacity-80 text-sm text-white rounded-lg gap-2" to={`/customer/add`}>
-                                <FiPlus className="text-xl text-white" />  Create new
-                            </Link>
-                        </> : <></>}
-
-
-
+                    <div>
+                        <h3 className="text-2xl font-semibold text-[#111827]">
+                            {shared.title}
+                        </h3>
+                        <p class="text-sm font-normal text-[#75757A]">Here you can see all about your {shared.title}</p>
                     </div>
 
+
+                    <div className="flex">
+                        <div className="buttons_Section filterFlex">
+
+                            <button className="!px-2.5 text-[#3C3E49] text-sm font-normal py-2.5 flex items-center justify-center gap-2 bg-[#fff] rounded-lg shadow-btn hover:bg-[#F3F2F5] border border-[#D0D5DD] transition-all focus:ring-2 ring-[#F1F2F3] disabled:bg-[#F3F2F5] disabled:cursor-not-allowed mr-3" onClick={() => exportfun()}>
+                                <PiFileCsv className="text-typo text-xl" />  Export CSV
+                            </button>
+                            {isAllow(`add${shared.check}`) ? <>
+                                <Link className="bg-primary leading-10 mr-3 h-10 flex items-center shadow-btn px-6 hover:opacity-80 text-sm text-white rounded-lg gap-2" to={`/customer/add`}>
+                                    <FiPlus className="text-xl text-white" />  Create new
+                                </Link>
+                            </> : <></>}
+
+
+
+                        </div>
+
+                    </div>
                 </div>
-            </div>
 
 
-            <div className='bg-white shadow-box rounded-lg w-full mt-6'>
-                <div className='selcitng p-4'>
-                    <div className='flex justify-between items-center gap-4'>
+                <div className='bg-white shadow-box rounded-lg w-full mt-6'>
+                    <div className='selcitng p-4'>
+                        <div className='flex justify-between items-center gap-4'>
 
-                        <div className='flex items-stretch gap-4'>
-                            {/* <div className='w-80'>
+                            <div className='flex items-stretch gap-4'>
+                                {/* <div className='w-80'>
                                 <SelectDropdown
                                     isSingle={true}
                                     id="plansDropdown"
@@ -266,7 +266,7 @@ const Html = ({
                                 />
 
                             </div> */}
-                           <SelectDropdown
+                                <SelectDropdown
                                     isSingle={true}
                                     id="statusDropdown"
                                     displayValue="name"
@@ -280,32 +280,32 @@ const Html = ({
                                     ]}
                                 />
                                 <SelectDropdown
-                            id="statusDropdown"
-                            displayValue="name"
-                            placeholder='All Groups'
-                            intialValue={filters.groupId}
-                            theme="search"
-                            result={e => filter({ groupId: e.value })}
-                            options={groups}
-                        />
+                                    id="statusDropdown"
+                                    displayValue="name"
+                                    placeholder='All Groups'
+                                    intialValue={filters.groupId}
+                                    theme="search"
+                                    result={e => filter({ groupId: e.value })}
+                                    options={groups}
+                                />
 
-<SelectDropdown
-                            id="statusDropdown"
-                            displayValue="name"
-                            placeholder='All Customer Role'
-                            intialValue={filters.customerRole}
-                            theme="search"
-                            result={e => filter({ customerRole: e.value })}
-                            options={roles}
-                        />
+                                <SelectDropdown
+                                    id="statusDropdown"
+                                    displayValue="name"
+                                    placeholder='All Customer Role'
+                                    intialValue={filters.customerRole}
+                                    theme="search"
+                                    result={e => filter({ customerRole: e.value })}
+                                    options={roles}
+                                />
 
-                        {filters.groupId||filters.status||filters.customerRole?<>
-                            <button className='bg-primary leading-10  h-10 shadow-btn px-6 hover:opacity-80 text-sm text-white rounded-lg ' onClick={reset}>Reset</button>
-                        </>:<></>}
+                                {filters.groupId || filters.status || filters.customerRole ? <>
+                                    <button className='bg-primary leading-10  h-10 shadow-btn px-6 hover:opacity-80 text-sm text-white rounded-lg ' onClick={reset}>Reset</button>
+                                </> : <></>}
 
-                        </div>
+                            </div>
 
-                        {/* <div className='flex '>
+                            {/* <div className='flex '>
                             <div className='w-80'>
                                 <DateRangePicker
                                     value={filters.startDate ? { startDate: new Date(filters.startDate), endDate: new Date(filters.endDate) } : { startDate: '', endDate: '' }}
@@ -314,51 +314,51 @@ const Html = ({
                                 />
                             </div>
                         </div> */}
+                        </div>
+
                     </div>
 
+                    {!loaging ? <>
+                        <Table
+                            className='mb-3'
+                            data={data}
+                            columns={columns}
+                            page={filters.page}
+                            count={filters.count}
+                            total={total}
+                            result={(e) => {
+                                if (e.event == 'page') pageChange(e.value)
+                                if (e.event == 'sort') sorting(e.value)
+                            }}
+                        />
+
+                    </> : <></>}
+
+                    {
+                        loaging ? <div className="text-center py-4">
+                            <img src="/assets/img/loader.gif" className="pageLoader" />
+                        </div> : <></>
+                    }
+
+
                 </div>
-
-                {!loaging ? <>
-                    <Table
-                        className='mb-3'
-                        data={data}
-                        columns={columns}
-                        page={filters.page}
-                        count={filters.count}
-                        total={total}
-                        result={(e) => {
-                            if (e.event == 'page') pageChange(e.value)
-                            if (e.event == 'sort') sorting(e.value)
+            </Layout>
+            {modal ? <>
+                <Modal
+                    title='Send Email'
+                    body={<SendEmail
+                        result={e => {
+                            if (e.event == 'submit') setModel(false)
                         }}
-                    />
+                    />}
+                    result={e => {
+                        if (e.event == 'close') setModel(false)
+                    }}
+                />
+            </> : <></>}
 
-                </> : <></>}
-
-                {
-                    loaging ? <div className="text-center py-4">
-                        <img src="/assets/img/loader.gif" className="pageLoader" />
-                    </div> : <></>
-                }
-
-
-            </div>
-        </Layout>
-        {modal?<>
-            <Modal
-            title='Send Email'
-            body={<SendEmail
-                result={e=>{
-                    if(e.event=='submit') setModel(false)
-                }}
-            />}
-            result={e=>{
-                if(e.event=='close') setModel(false)
-            }}
-            /> 
-        </>:<></>}
-       
         </>
-       
+
     );
 };
 
