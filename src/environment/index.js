5670bb3864591f6afa18465b8b91c5bae6f319da
Ritika <dev.jc.18@gmail.com>
2024-10-23 11:16:19 +0530

    pull from staging branch

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,5 +1,5 @@
 let host = document.location.host;
-let apiUrl ='https://skillstaging.azurewebsites.net/'
+let apiUrl ='https://skillprobackend.azurewebsites.net/'
 const environment = {
     api: apiUrl,
     sasKey:'sp=racwdli&st=2024-05-01T12:17:30Z&se=2070-05-01T20:17:30Z&sv=2022-11-02&sr=c&sig=AQ7v4HhuDj%2BeA0pZHQEy2zATZKSwBMpjie%2BhvcaLc6Y%3D',
@@ -7,7 +7,7 @@ const environment = {
     container:'exel',
      map_api_key:'AIzaSyCbRhC6h9Pp43-5t_Knyrd_ewAdLMIJtCg',
     planTypeId:'65ead4e65cfbfd7a03ed102f',
-    apiUrl :'https://skillstaging.azurewebsites.net/',
+    apiUrl :'https://skillprobackend.azurewebsites.net/',
     userRoleId:'65eac23ceac028f4dbfb1fbc',
     customerRoleId:'66026ea2f105ac23ab31e48e',
     glRoleId:'65fc4a1e2e22cff912335e12',
